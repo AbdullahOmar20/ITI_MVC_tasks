337e20f9d189df78af3a8a79e0b00b0dc169ef02
abdullah <abdullahamer323@gmail.com>
2023-09-25 16:53:50 +0300

    day 3 (update ,delete and layout + the bonus)

--- ITI_MVC/Controllers/EmployeesController.cs
+++ ITI_MVC/Controllers/EmployeesController.cs
@@ -23,15 +23,51 @@ namespace ITI_MVC.Controllers
             {
                 return Content("Error");
             }
+            
             return View(employee);
         }
         public IActionResult Add() 
         { 
+            ViewBag.Office = context.office.ToList();
             return View();
         }
         public IActionResult Insert(Employee emp) 
         {
             context.employees.Add(emp);
+            context.SaveChanges();
+            return RedirectToAction("information");
+        }
+        public IActionResult Edit(int id )
+        {
+            Employee emp = context.employees.Where(i=>i.Id == id).SingleOrDefault();
+			if (emp == null)
+			{
+				return Content("Error");
+			}
+			ViewBag.Office = context.office.ToList();
+            return View(emp);
+        }
+        public IActionResult EditONDB(Employee emp)
+        {
+            Employee e = context.employees.SingleOrDefault(i => i.Id == emp.Id);
+            e.Name = emp.Name;
+            e.Salary= emp.Salary;
+            e.Adress= emp.Adress;
+            e.department = emp.department;
+            e.Office_Id= emp.Office_Id;
+            context.SaveChanges();
+            return RedirectToAction("information");
+        }
+        public IActionResult Delete(int id)
+        {
+            Employee emp = context.employees.SingleOrDefault(i=>i.Id == id);
+            Emp_Project emp_pro = context.emp_Projs.SingleOrDefault(i => i.Emp_Id == id);
+            if(emp_pro != null) 
+            {
+				context.emp_Projs.Remove(emp_pro);
+			}
+            
+            context.employees.Remove(emp);
             context.SaveChanges();
             return RedirectToAction("information");
         }
